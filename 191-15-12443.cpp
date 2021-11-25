59aa2738bb00123dd55e5bc85569fb58326464be
Siddiquenahian <nahian198@gmail.com>
2021-11-25 20:13:56 +0600

    This is first version

---
+++ 191-15-12443.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+#include<string.h>
+#include "stdio.h"
+
+using namespace std;
+int main()
+{
+    string s;
+    char str[100];
+    int i=0, length, j;
+    cout <<"Enter a string: ";
+    getline(cin, s, '*');
+    gets(str);
+    length=strlen(str);
+
+    for(i=0; i<length; i++){
+        if(str[i]==' '){
+            for(j=i; j<length; j++)
+                str[j]=str[j+1];
+            length--;
+            }
+
+        }
+        cout << ' ' << str;
+
+    }
